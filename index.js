f45e5423a362e6a2bd51a34c892509a04324be42
Celeste <celeste-quinn@live.nmit.ac.nz>
2020-04-06 14:48:56 +1200

    added extra stuff

--- index.js
+++ index.js
@@ -58,11 +58,20 @@ function foo(x, y, z, d, ...args)
 
 foo(1, 2, 3, 4, 5, 6, 7)  //so you can write the letter d in there to add the number 4 into the parameters instead of being part of the array
 
-function asd(...args) //instead of using variables like x y x, we use ...args (spread operator)
-{                     //and it can hold the all these indexes that you can write down there
-    console.log(args[5]) //so the spread operator is going through the array and finding the 5th index, which is 6, since index starts at zero
+// function asd(...args) //instead of using variables like x y x, we use ...args (spread operator)
+// {                     //and it can hold the all these indexes that you can write down there
+//     console.log(args[5]) //so the spread operator is going through the array and finding the 5th index, which is 6, since index starts at zero
+// }
+
+// var arr = [1, 2, 3, 4, 5, 6, 7]
+
+// asd(...arr)
+
+
+function test(...args)
+{
+    console.log(args[2])
 }
 
-var arr = [1, 2, 3, 4, 5, 6, 7]
-
-asd(...arr)
+var args = [24, 62, 43, 56]
+test(...args)
